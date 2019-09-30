7290ee10560354923dbc2ab52c9736940bbd3d71
Karen Giovanna Melo <49700354+karengiovanna@users.noreply.github.com>
2019-09-30 09:29:31 -0400

    adicionei a função create list

--- Lista Encadeada/lista.c
+++ Lista Encadeada/lista.c
@@ -1,3 +1,14 @@
+#include <stdio.h> // entrada e saida
+#include <stdlib.h> // alocacao dinamica
+#include "lista.h" //importa a biblioteca que criamos
+
+List* create_list(){
+  List *lista = (List *) malloc (sizeof(List)); //cria um lista com o tamanho do struct List
+  lista->head = NULL; // o head da lista criada eh null
+  lista->len = 0; // o tamanho da lista criada eh 0
+  return lista; // retorna o ponteido de onde esta a lista 
+}
+
 void nodos_list_destroy(Nodo *head){
   if (head!=NULL){
     nodos_list_destroy(head->next); // recursividade 
