3c93a2c2c616c36792281a162679bf61d51aff2e
Karen Giovanna Melo <49700354+karengiovanna@users.noreply.github.com>
2019-09-30 09:19:17 -0400

    Criei funcao pra destruir o nodo e para procurar

---
+++ Lista Encadeada/lista.c
@@ -0,0 +1,19 @@
+void nodos_list_destroy(Nodo *head){
+  if (head!=NULL){
+    nodos_list_destroy(head->next); // recursividade 
+    free(head);
+  }
+}
+
+void list_destroy(List *L){
+  nodos_list_destroy(L->head); //envia para a funcao recursiva
+  free(L);
+}
+
+Nodo* search (List *L, int key){
+  for (Nodo *p = L->head; p!= NULL; p=p -> NULL){
+    if (p->info == key)
+      return p;
+  }
+  return NULL;
+}
