75b9dd85125ec153a67e54791c9e786e9eb6d80c
Karen <giovannagiovanna13@gmail.com>
2019-10-21 09:59:59 -0400

    Update

--- Lista Encadeada/lista.c
+++ Lista Encadeada/lista.c
@@ -2,13 +2,45 @@
 #include <stdlib.h> // alocacao dinamica
 #include "lista.h" //importa a biblioteca que criamos
 
-List* create_list(){
-  List *lista = (List *) malloc (sizeof(List)); //cria um lista com o tamanho do struct List
+Lista * criar_lista(){
+  Lista *lista = (Lista *) malloc (sizeof(Lista)); //cria um lista com o tamanho do struct List
   lista->head = NULL; // o head da lista criada eh null
   lista->len = 0; // o tamanho da lista criada eh 0
   return lista; // retorna o ponteido de onde esta a lista 
 }
 
+void inserir_lista(Lista *L, int v){
+  Nodo *new = (Nodo*) malloc(sizeof(Nodo)); // cria um novo nodo
+  L->len++; // aumenta o tamanho da lista
+  new->info = v; //o valor será inserido no novo nodo
+  new->next = L->head;
+  L->head = new; //novo nodos sempre são inseridos no começo da lista
+}
+
+int esta_vazia(Lista *L){
+  return (L->head == NULL);
+}
+
+/*
+* list_destroy_iterativo não estará visível para o usuário
+porque não adicionamos o cabeçalho dela no list.h
+*/
+void list_destroy_interativo(Lista *L){
+  Nodo *comeco, *anterior;
+  comeco = L-> head;
+  while (comeco != NULL){
+    anterior = comeco;
+    comeco = comeco -> next;
+    free(anterior);
+  } 
+  free(L);
+}
+
+/*
+* Nodos list Destroy nao estará visível para
+o usuário porque não adicionamos o cabeçalho
+dela no list.h
+*/
 void nodos_list_destroy(Nodo *head){
   if (head!=NULL){
     nodos_list_destroy(head->next); // recursividade 
@@ -16,18 +48,24 @@ void nodos_list_destroy(Nodo *head){
   }
 }
 
-void list_destroy(List *L){
+void list_destroy(Lista *L){
   nodos_list_destroy(L->head); //envia para a funcao recursiva
   free(L);
 }
 
-Nodo* search (List *L, int key){
+Nodo* procurar (Lista *L, int key){
   for (Nodo *p = L->head; p!= NULL; p=p -> NULL){
     if (p->info == key)
       return p;
   }
   return NULL;
 }
+
+/*
+* search_nodo não estará visível para o usuário
+porque não adicionamos o cabeçalho dela no list.h
+*/
+Nodo * procurar_nodo(Nodo* head, int key)
 
 Nodo *search_nodo(Nodo *head){
   if (head == NULL) //verifica se é nulo, se for, nao achei
@@ -42,4 +80,13 @@ Nodo *search_recursivo(List *L, int key){
 }
 
 
-void list_print(List *L);
+void list_print(Lista *L){
+  for (Nodo *posicao = L-> head; posicao!= NULL;posicao=posicao->next){
+    printf("%d", posicao-> info);
+  }
+  printf("\n");
+}
+
+void list_remove(Lista *L, int v){
+  
+}
--- Lista Encadeada/lista.h
+++ Lista Encadeada/lista.h
@@ -4,9 +4,19 @@
 typedef struct nodo{
   int info;
   struct nodo *next;
-}Nodo;
+}Nodo; 
 
-typedef struct list{
+typedef struct lista{
   Nodo *head;
   int len;
-} List;
+} Lista;
+
+Lista * criar_lista();
+void inserir_lista(Lista*, int v);
+int esta_vazia(Lista *L);
+void destruir_lista(Lista *L);
+Nodo* procurar_nodo(Lista *L, int chave);
+void imprimir_lista(Lista *L);
+void remover_lista(Lista *L, int v);
+
+#endif
